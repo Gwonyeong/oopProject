45bc28ede54991167f5e778968ec07bfedadc7ea
gwonyeong <qwe980121@naver.com>
2023-08-13 22:07:22 +0900

    Add: charcter class

---
+++ character.js
@@ -0,0 +1,9 @@
+class Character {
+    constructor(name, hp, attack, defense, job) {
+        this.name = name;
+        this.hp = hp;
+        this.attack = attack;
+        this.defense = defense;
+        this.job = job;
+    }
+}
